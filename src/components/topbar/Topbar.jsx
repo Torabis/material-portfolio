e3de6562486fb6b12f467c52987033e650e0c19e
Torabis <sadihome@gmail.com>
2021-05-17 21:13:17 -0700

    style topbar & menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,28 @@
 import './topbar.scss'
-
-export default function Topbar() {
+import { Person, Mail } from "@material-ui/icons";
+export default function Topbar({menuOpen, setMenuOpen}) {
     return (
-        <div className="topbar">
-            
+        <div className={"topbar " + (menuOpen && 'active')}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro"  className='logo'> CODART</a>
+                    <div className="itemContainer">
+                        <Person className='icon'/>
+                        <span>+1 949 405 6663</span>
+                        <div className="itemContainer">
+                        <Mail className='icon'/>
+                        <span>codart.us@gmail.com</span>
+                    </div>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span classNamene= "line1"></span>
+                        <span classNamene= "line2"></span>
+                        <span classNamene= "line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
